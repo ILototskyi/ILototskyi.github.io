5fd1f6f7e29ecebc47e755005396fa84e5f52329
Ilya <z3prodigy07@gmail.com>
2020-09-19 04:06:31 +0300

    add script

--- src/js/script.js
+++ src/js/script.js
@@ -1,4 +1,8 @@
-function buttonClick() {
-	var input = document.getElementById('input');
-	input.value = '!!!';
+"use strict";
+
+document.querySelector("button").onclick = myClick;
+
+function myClick() {
+    let a = document.querySelector(".myInput").value;
+    document.querySelector(".textOutput").innerHTML = a;
 }
