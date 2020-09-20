0e248e44a085612bf6cf8202ba3849db497eda5f
Ilya <z3prodigy07@gmail.com>
2020-09-20 19:52:10 +0300

    add function

--- src/js/script.js
+++ src/js/script.js
@@ -4,6 +4,28 @@ document.querySelector("button").onclick = myClick;
 
 function myClick() {
     let a = document.querySelector(".myInput").value;
-    document.querySelector(".textOutput").innerHTML = a;
+    document.querySelector(".textOutput").innerHTML = prepare(a);
     document.querySelector(".myInput").value = '';
 }
+
+function prepare(text) {
+    return text
+      .split(' ')
+      .map(word => {
+        if(word.length === 0) {
+            return word;
+        }
+        let classes = [];
+
+        if(word.length > 3) {
+            classes.push('three-chr');
+        }
+
+        if(word.match(/[!@#$&*%]/)) {
+            classes.push('special-chr');
+        }
+    
+        classes = classes.join(' ');
+        return `<span class="${classes}">${word}</span>`;
+      }).join(' ');
+  }
