9b7033325c61a20c9d2bbc960d90da941ca832a2
Ilya <z3prodigy07@gmail.com>
2020-09-24 00:01:38 +0300

    final adjustments

--- src/js/script.js
+++ src/js/script.js
@@ -1,24 +1,24 @@
 "use strict";
 
-  first("button").addEventListener("click", function() {
-    let val = first(".myInput").value.trim(); {
-    if (!val) { return; }
-  }
-    first(".textOutput").insertAdjacentHTML("beforeEnd", prepare(val));
-    first(".myInput").value = '';
-  });
-  
+first("button").addEventListener("click", function() {
+  let val = first(".myInput").value.trim(); {
+  if (!val) { return; }
+}
+  first(".textOutput").insertAdjacentHTML("beforeEnd", prepare(val));
+  first(".myInput").value = '';
+});
 
-  function prepare(text) {
-    return "<li>" + text.split(/\s+/).map(wrapSpan).join(" ") + "</li>";
 
-    function wrapSpan(word) {    
-      let color = /[!@#$&*%]/.test(word) ? "red" : word.length > 3 ? "green" : "";
-  
-      return `<span style="color: ${ color }">${ word }</span>`;
-    }
+function prepare(text) {
+  return "<li>" + text.split(/\s+/).map(wrapSpan).join(" ") + "</li>";
+
+  function wrapSpan(word) {    
+    let color = /[!@#$&*%]/.test(word) ? "red" : word.length > 3 ? "green" : "";
+
+    return `<span style="color: ${ color }">${ word }</span>`;
   }
-  
-  function first(str) {
-    return document.querySelector(str);
-  }
+}
+
+function first(str) {
+  return document.querySelector(str);
+}
